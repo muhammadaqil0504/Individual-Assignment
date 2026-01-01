0dd3c99fab70a84e684bbbe6e869be68ebad5ace
Aqil <muhammadaqil0504@gmail.com>
2026-01-01 22:25:39 +0800

    Update README with students details

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    supabaseUrl: 'https://tjcmpipiqgrxsdvrpctd.supabase.co',
+    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRqY21waXBpcWdyeHNkdnJwY3RkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY4NDI5NzcsImV4cCI6MjA4MjQxODk3N30.qrYv11N3e2hwLzy5zbnM2tNP0mYjhHaFcFsCrJEm7a8',
+};
